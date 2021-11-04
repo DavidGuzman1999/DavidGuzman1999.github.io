a1b3cb3b91da3b2a469777f855fac985a55b75ac
David Guzman <dguzman1999@gmail.com>
2021-11-04 15:54:39 -0400

    adding js again to see if it works

--- weather_js.js
+++ weather_js.js
@@ -1,34 +1,37 @@
-$(document).ready(function(){
-    // get location
+$(document).ready(function () {
+
+    // Get Location
     navigator.geolocation.getCurrentPosition(success, error);
 
-    function success (pos) {
+    function success(pos) {
         var lat = pos.coords.latitude;
         var long = pos.coords.longitude;
-        weather (lat, long);
+        weather(lat, long);
     }
-    function error (){
-        console.log("error");
+
+    function error() {
+        console.log('There was an error');
     }
-   function weather () {
-       var url = 'https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}';
-       $.getJSON (URL, function(){
-           console.log(data);
-           updateDOM(data)
-       });
-   }
 
-   weather();
-   function updateDOM (data) {
-       var city = data.name;
-       var temp = Math.round(data.main.temp);
-       var desc = data.weather[0].description;
-       var icon = data.weather[0].icon;
+    // Call Weather
+    function weather(lat, long) {
+        var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
-       $("#city").html(city);
-       $("#temp").html(temp);
-       $("#desc").html(desc);
-       $("#icon").attr("src", icon);
+        $.getJSON(URL, function(data) {
+            updateDOM(data);
+        });
+    }
 
-   }
+    // Update Dom
+    function updateDOM(data) {
+        var city = data.name;
+        var temp = Math.round(data.main.temp_max);
+        var desc = data.weather[0].description;
+        var icon = data.weather[0].icon;
+
+        $('#city').html(city);
+        $('#temp').html(temp);
+        $('#desc').html(desc);
+        $('#icon').attr('src', icon);
+    }
 });
